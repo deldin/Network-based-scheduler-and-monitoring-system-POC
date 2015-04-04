e2b91bde0bce3ee59d691e0675081b3cb9bc780a
Teja <raviteja.prabhala@gmail.com>
2015-04-04 17:05:37 -0500

    Added Initial Web App project

---
+++ ScheduleMonitorApp/ScheduleMonitorApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ScheduleMonitorApp.MvcApplication" Language="C#" %>
---
+++ ScheduleMonitorApp/ScheduleMonitorApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScheduleMonitorApp.Startup))]
+namespace ScheduleMonitorApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
