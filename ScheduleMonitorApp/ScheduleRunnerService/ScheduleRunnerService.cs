21c3a946f44e985ec6b33a1300a88011f1ddf82f
Teja <raviteja.prabhala@gmail.com>
2015-04-05 20:17:46 -0500

    Added Windows Service and Console Client and Added Log4Net logging in Service

---
+++ ScheduleMonitorApp/ScheduleRunnerService/ScheduleRunnerService.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace ScheduleRunnerService
+{
+    public partial class ScheduleRunnerService : ServiceBase
+    {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private Thread m_serviceThread;
+        private AutoResetEvent m_done = new AutoResetEvent(false);
+        private ManualResetEvent m_running = new ManualResetEvent(true);
+        public ScheduleRunnerService()
+        {
+            
+            log4net.Config.XmlConfigurator.Configure();
+            System.Diagnostics.EventLog.WriteEntry("ScheduleRunnerService", DateTime.Now.ToLongTimeString() + " Started in the constructor");
+            Log.Info("Service has started in the constructor");
+            InitializeComponent();
+
+            this.CanHandlePowerEvent = true;
+            this.CanHandleSessionChangeEvent = true;
+            this.CanPauseAndContinue = true;
+            this.CanShutdown = true;
+            this.CanStop = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log.Info("Service has started");
+
+            // Kick off a thread to do work
+            m_serviceThread = new Thread(new ThreadStart(ThreadProcess));
+            m_serviceThread.Start();
+
+
+            //log4net.Config.XmlConfigurator.Configure();
+            Log.Info("Service has Second started");
+            System.Diagnostics.EventLog.WriteEntry("ScheduleRunnerService", DateTime.Now.ToLongTimeString() + " Started");
+            base.OnStart(args);
+        }
+
+        /// <summary>
+        /// The background thread.
+        /// </summary>
+        private void ThreadProcess()
+        {
+
+            while (!m_done.WaitOne(1000, false))
+            {
+                if (m_running.WaitOne(0, false))
+                {
+                    // Log4Net logging methods.
+                    //Log.Debug("This is the debug message");
+                    //Log.Info("This is the info message");
+                    //Log.Warn("This is the warn message");
+                    //Log.Error("This is the error message");
+                    //Log.Fatal("This is the fatal message");
+
+                    //// Standard .NET tracing methods.
+                    //Debug.WriteLine("This is .NET Debug");
+                    //Trace.WriteLine("This is .NET Trace");
+                }
+                else
+                {
+                    // Wait to be signalled
+                    m_running.WaitOne();
+                }
+            }
+        }
+
+        //protected override void OnStop()
+        //{
+        //    base.OnStop();
+        //    System.Diagnostics.EventLog.WriteEntry("ScheduleRunnerService", DateTime.Now.ToLongTimeString() + " Stopped ");
+        //    Log.Info("Service has Stopped");
+        //}
+
+        protected override void OnCustomCommand(int command)
+        {
+            try
+            {
+                base.OnCustomCommand(command);
+                System.Diagnostics.EventLog.WriteEntry("ScheduleRunnerService", DateTime.Now.ToLongTimeString() + " Custom command recieved " + command);
+                //log4net.LogicalThreadContext.Properties["CommandValue"] = command.ToString();
+                Log.Info("Ran custom command : " + command);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error while running custom command : " + command + " " + ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ ScheduleMonitorApp/SchedulerClient/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace SchedulerClient
+{
+    class Program
+    {
+        private enum SimpleServiceCustomCommands { StopWorker = 128, RestartWorker, CheckWorker };
+        static void Main(string[] args)
+        {
+            ServiceController myService = new ServiceController("ScheduleRunnerService");
+            myService.ExecuteCommand((int)SimpleServiceCustomCommands.StopWorker);
+            myService.ExecuteCommand((int)SimpleServiceCustomCommands.RestartWorker);
+            myService.ExecuteCommand((int)SimpleServiceCustomCommands.CheckWorker);
+
+
+        }
+        //public enum SimpleServiceCustomCommands
+        //{ StopWorker = 128, RestartWorker, CheckWorker };
+        //static void Main(string[] args)
+        //{
+        //    ServiceController[] scServices;
+        //    scServices = ServiceController.GetServices();
+
+        //    foreach (ServiceController scTemp in scServices)
+        //    {
+
+        //        if (scTemp.ServiceName == "ScheduleRunnerService")
+        //        {
+        //            // Display properties for the Simple Service sample 
+        //            // from the ServiceBase example.
+        //            ServiceController sc = new ServiceController("ScheduleRunnerService");
+        //            Console.WriteLine("Status = " + sc.Status);
+        //            Console.WriteLine("Can Pause and Continue = " + sc.CanPauseAndContinue);
+        //            Console.WriteLine("Can ShutDown = " + sc.CanShutdown);
+        //            Console.WriteLine("Can Stop = " + sc.CanStop);
+        //            if (sc.Status == ServiceControllerStatus.Stopped)
+        //            {
+        //                sc.Start();
+        //                while (sc.Status == ServiceControllerStatus.Stopped)
+        //                {
+        //                    Thread.Sleep(1000);
+        //                    sc.Refresh();
+        //                }
+        //            }
+        //            // Issue custom commands to the service 
+        //            // enum SimpleServiceCustomCommands  
+        //            //    { StopWorker = 128, RestartWorker, CheckWorker };
+        //            sc.ExecuteCommand((int)SimpleServiceCustomCommands.StopWorker);
+        //            sc.ExecuteCommand((int)SimpleServiceCustomCommands.RestartWorker);
+        //            sc.ExecuteCommand((int)SimpleServiceCustomCommands.CheckWorker);
+                    
+        //            Console.WriteLine("Status = " + sc.Status);
+        //            // Display the event log entries for the custom commands 
+        //            // and the start arguments.
+        //            EventLog el = new EventLog("Application");
+        //            EventLogEntryCollection elec = el.Entries;
+        //            foreach (EventLogEntry ele in elec)
+        //            {
+        //                if (ele.Source.IndexOf("SimpleService.OnCustomCommand") >= 0 |
+        //                    ele.Source.IndexOf("SimpleService.Arguments") >= 0)
+        //                    Console.WriteLine(ele.Message);
+        //            }
+        //        }
+        //    }
+
+
+        //}
+    }
+}
